b6fe38064ebe252c09a0c1c85cdf4edc700acbc1
bluesea0109 <ub2099236@gmail.com>
2020-04-29 14:05:20 -0600

    fix lint errors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import 'firebase/auth';
 import { useSelector } from "react-redux";
 import { getCurrentUser } from "../store/selectors"
-import { Grid, makeStyles, Theme, createStyles, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-  }));
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//   }));
 
 export default function Home(props: any) {
   const user = useSelector(getCurrentUser);
