c8fdfbc011426504a54ac6a765b7c346c19575d2
bluesea0109 <ub2099236@gmail.com>
2020-05-22 14:15:03 -0600

    Fix lint errors

--- src/components/ChatbotBuilder/NewAgent.tsx
+++ src/components/ChatbotBuilder/NewAgent.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme: Theme) =>
     button: {
       margin: theme.spacing(1),
     },
-  })
+  }),
 );
 
 interface INewAgentProps {
--- src/components/ChatbotBuilder/index.tsx
+++ src/components/ChatbotBuilder/index.tsx
@@ -12,7 +12,7 @@ const useStyles = makeStyles((theme: Theme) =>
     paper: {
       padding: theme.spacing(2),
     },
-  })
+  }),
 );
 
 interface IChatbotBuilderProps {
