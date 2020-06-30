c2a25e2c70f775e902999518672be78d582d8157
Anshul Sanghi <anshap1719@gmail.com>
2020-07-01 03:04:35 +0530

    Fix ESLint Errors In CircleCI

--- src/components/Dashboard/ProjectSettings/index.tsx
+++ src/components/Dashboard/ProjectSettings/index.tsx
@@ -1,3 +1,4 @@
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import {
   Box,
   Button,
@@ -9,28 +10,28 @@ import {
   Tooltip,
   Typography,
 } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Add } from '@material-ui/icons';
-import { useMutation, useQuery } from '@apollo/react-hooks';
-import { createApiKeyMutation, deleteApiKeyMutation, getApiKeysQuery } from './gql';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+
+import { createApiKeyMutation, deleteApiKeyMutation, getApiKeysQuery } from './gql';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     keyItem: {
-      margin: theme.spacing(2, 0)
+      margin: theme.spacing(2, 0),
     },
     deleteBtn: {
       background: theme.palette.error.main,
       color: '#fff',
 
-      "&:hover": {
-        background: theme.palette.error.light
-      }
-    }
-  })
-)
+      '&:hover': {
+        background: theme.palette.error.light,
+      },
+    },
+  }),
+);
 
 export default function Project() {
   const classes = useStyles();
@@ -39,58 +40,58 @@ export default function Project() {
 
   const apiKeysQuery = useQuery(getApiKeysQuery, {
     variables: {
-      projectId
-    }
+      projectId,
+    },
   });
 
   const [addKey, createKeyMutation] = useMutation(createApiKeyMutation);
   const [deleteKey, deleteKeyMutation] = useMutation(deleteApiKeyMutation);
 
-  const loadedKey = apiKeysQuery.data?.apiKey.key ?? null
+  const loadedKey = apiKeysQuery.data?.apiKey.key ?? null;
 
   useEffect(() => {
     if (!apiKeysQuery.loading) {
       setCurrentKey(loadedKey);
     }
-  }, [loadedKey, apiKeysQuery.loading])
+  }, [loadedKey, apiKeysQuery.loading]);
 
   const createNewKey = async () => {
     try {
       const { data } = await addKey({
         variables: {
-          projectId
-        }
+          projectId,
+        },
       });
 
       setCurrentKey(data?.generateApiKey?.key ?? null);
     } catch (e) {
 
     }
-  }
+  };
 
   const deleteApiKey = async () => {
     try {
       await deleteKey({
         variables: {
-          projectId
-        }
+          projectId,
+        },
       });
 
       setCurrentKey(null);
     } catch (e) {
 
     }
-  }
+  };
 
   const loading = apiKeysQuery.loading || createKeyMutation.loading || deleteKeyMutation.loading;
 
   return (
     <Box p={3}>
-      <Grid container alignItems="center">
-        <Grid item>
+      <Grid container={true} alignItems="center">
+        <Grid item={true}>
           <Typography variant="h5">API Keys</Typography>
         </Grid>
-        <Grid item>
+        <Grid item={true}>
           <IconButton style={{ marginLeft: 16 }} onClick={createNewKey} disabled={loading}>
             <Add />
           </IconButton>
@@ -98,23 +99,23 @@ export default function Project() {
       </Grid>
       {(!currentKey && loading) && (
         <Box p={4}>
-          <Grid container alignItems="center" justify="center">
-            <Grid item>
+          <Grid container={true} alignItems="center" justify="center">
+            <Grid item={true}>
               <CircularProgress size={18} />
             </Grid>
           </Grid>
         </Box>
       )}
       {currentKey && (
-        <Grid container className={classes.keyItem} justify="space-between" alignItems="center">
-          <Grid item>
-            <Tooltip title="Copy Key" arrow>
+        <Grid container={true} className={classes.keyItem} justify="space-between" alignItems="center">
+          <Grid item={true}>
+            <Tooltip title="Copy Key" arrow={true}>
               <Typography style={{ cursor: 'pointer' }}>
                 {currentKey}
               </Typography>
             </Tooltip>
           </Grid>
-          <Grid item>
+          <Grid item={true}>
             <Button className={classes.deleteBtn} disabled={loading} onClick={deleteApiKey}>Delete</Button>
           </Grid>
         </Grid>
