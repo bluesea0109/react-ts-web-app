3270bfde932cc2212a8108c1eec7737a74f9471c
Sergei <praveenkoka@gmail.com>
2020-08-06 21:39:14 +0530

    removed comments

--- src/context/OptionImages.tsx
+++ src/context/OptionImages.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import {IOptionImage} from '../models/chatbot-service';
 
-// We define our type for the context properties right here
+
 type OptionImagesContextProps = {
   optionImages: IOptionImage[],
 };
 
-// we initialise them without default values, to make that happen, we
-// apply the Partial helper type.
-export const OptionImagesContext =
-  React.createContext<Partial<OptionImagesContextProps>>({});
+export const OptionImagesContext = React.createContext<Partial<OptionImagesContextProps>>({});
