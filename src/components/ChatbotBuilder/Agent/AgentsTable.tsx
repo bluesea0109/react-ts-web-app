379c5f3533e279ee648fe37799b6045edba00a5e
kokaqux <kokaqux@gmail.com>
2020-08-31 22:36:52 +0530

    removed name

--- src/components/ChatbotBuilder/Agent/AgentsTable.tsx
+++ src/components/ChatbotBuilder/Agent/AgentsTable.tsx
@@ -7,7 +7,11 @@ import MaterialTable, { Column } from 'material-table';
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import { CHATBOT_DELETE_AGENT, CHATBOT_GET_AGENTS, CHATBOT_UPDATE_AGENT } from '../../../common-gql-queries';
+import {
+  CHATBOT_DELETE_AGENT,
+  CHATBOT_GET_AGENTS,
+  CHATBOT_UPDATE_AGENT,
+} from '../../../common-gql-queries';
 import { IAgent } from '../../../models/chatbot-service';
 import ApolloErrorPage from '../../ApolloErrorPage';
 import ContentLoading from '../../ContentLoading';
@@ -35,7 +39,9 @@ interface AgentState {
 function AgentsTable() {
   const classes = useStyles();
   const { projectId, orgId } = useParams();
-  const agentsData = useQuery<IGetAgents>(CHATBOT_GET_AGENTS, { variables: { projectId } });
+  const agentsData = useQuery<IGetAgents>(CHATBOT_GET_AGENTS, {
+    variables: { projectId },
+  });
   const [deleteAgent, { loading, error }] = useMutation(CHATBOT_DELETE_AGENT, {
     refetchQueries: [{ query: CHATBOT_GET_AGENTS, variables: { projectId } }],
     awaitRefetchQueries: true,
@@ -44,18 +50,21 @@ function AgentsTable() {
     refetchQueries: [{ query: CHATBOT_GET_AGENTS, variables: { projectId } }],
     awaitRefetchQueries: true,
   });
-  const agents: IAgent[] | undefined = agentsData && agentsData.data && agentsData.data.ChatbotService_agents;
+  const agents: IAgent[] | undefined =
+    agentsData && agentsData.data && agentsData.data.ChatbotService_agents;
   const [state, setState] = React.useState<AgentState>({
     columns: [
       { title: 'Agent id', field: 'id', editable: 'never' },
-      { title: 'Unique Name', field: 'uname', editable: 'never' },
       {
-        title: 'Name',
-        field: 'name',
-        render: rowData => <Link to={`/orgs/${orgId}/projects/${projectId}/chatbot-builder/agents/${rowData.id}/Actions`}>
-          {rowData.name}
-        </Link>,
-        editable: 'onUpdate',
+        title: 'Unique Name',
+        field: 'uname',
+        editable: 'never',
+        render: (rowData) => (
+          <Link
+            to={`/orgs/${orgId}/projects/${projectId}/chatbot-builder/agents/${rowData.id}/Actions`}>
+            {rowData.name}
+          </Link>
+        ),
       },
       { title: 'Language', field: 'language', editable: 'never' },
     ],
@@ -70,10 +79,14 @@ function AgentsTable() {
       });
     }
 
-    return () => { };
+    return () => {};
   }, [agents, state.columns]);
 
-  const commonError = agentsData.error ? agentsData.error : updatedData.error ? updatedData.error : error;
+  const commonError = agentsData.error
+    ? agentsData.error
+    : updatedData.error
+    ? updatedData.error
+    : error;
 
   if (agentsData.loading || updatedData.loading || loading) {
     return <ContentLoading />;
@@ -112,7 +125,6 @@ function AgentsTable() {
             options={{
               actionsColumnIndex: -1,
             }}
-
             localization={{
               body: {
                 editRow: {
@@ -136,10 +148,10 @@ function AgentsTable() {
           />
         </TableContainer>
       ) : (
-          <Typography align="center" variant="h6">
-            {'No Agents found'}
-          </Typography>
-        )}
+        <Typography align="center" variant="h6">
+          {'No Agents found'}
+        </Typography>
+      )}
     </Paper>
   );
 }
