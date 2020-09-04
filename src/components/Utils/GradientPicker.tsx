4ebab474969a218cb34d284f06feca7b1ed2d6dd
kokaqux <kokaqux@gmail.com>
2020-09-04 15:02:07 +0530

    wip nearly done

--- src/components/Utils/GradientPicker.tsx
+++ src/components/Utils/GradientPicker.tsx
@@ -1,5 +1,27 @@
-import { Box, Slider, Typography } from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Slider,
+  Tooltip,
+  Typography,
+} from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  withStyles,
+} from '@material-ui/core/styles';
+import {
+  Add,
+  AllOut,
+  ArrowDownward,
+  ArrowForward,
+  CallMade,
+  CallReceived,
+  Delete,
+} from '@material-ui/icons';
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { ColorResult, SketchPicker } from 'react-color';
@@ -20,6 +42,47 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundColor: theme.palette.background.default,
       padding: theme.spacing(2),
     },
+    point: {
+      width: 10,
+      height: 10,
+      top: 9,
+      position: 'absolute',
+      boxShadow: '0px 0px 2px 1px #999',
+      zIndex: 1,
+    },
+    relativeContainer: {
+      position: 'relative',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    thumbIcon: {
+      position: 'absolute',
+      fontSize: 15,
+      zIndex: 2,
+      color: theme.palette.grey[500],
+    },
+    addButton: {
+      position: 'absolute',
+      bottom: 5,
+      left: 'calc(50% - 20px)',
+      backgroundColor: 'rgba(255,255,255,.7)',
+      color: theme.palette.grey[500],
+      '&:hover': {
+        backgroundColor: 'rgba(255,255,255,1)',
+      },
+    },
+    gradientTypeButton: {
+      position: 'absolute',
+      left: 5,
+      top: 5,
+      backgroundColor: 'rgba(255,255,255,.7)',
+      color: theme.palette.grey[500],
+      '&:hover': {
+        backgroundColor: 'rgba(255,255,255,1)',
+      },
+    },
+    sketchPicker: {},
   }),
 );
 
@@ -58,15 +121,24 @@ interface IGradientPoint {
   };
 }
 
+type GradientDirection =
+  | 'to bottom'
+  | 'to right'
+  | '135deg'
+  | '45deg'
+  | 'ellipse at center';
+
 interface IGradientSpec {
-  direction:
-    | 'to bottom'
-    | 'to right'
-    | '135deg'
-    | '45deg'
-    | 'ellipse at center';
+  direction: GradientDirection;
   points: IGradientPoint[];
 }
+
+const BLACK = {
+  r: 0,
+  g: 0,
+  b: 0,
+  a: 1,
+};
 
 const DEFAULT_GRADIENT: IGradientSpec = {
   direction: 'to bottom',
@@ -77,7 +149,7 @@ const DEFAULT_GRADIENT: IGradientSpec = {
         r: 0,
         g: 0,
         b: 0,
-        a: 1,
+        a: 0.1,
       },
     },
     {
@@ -92,19 +164,34 @@ const DEFAULT_GRADIENT: IGradientSpec = {
   ],
 };
 
+const directionIconMap = {
+  'to bottom': <ArrowDownward />,
+  'to right': <ArrowForward />,
+  '45deg': <CallMade />,
+  '135deg': <CallReceived />,
+  'ellipse at center': <AllOut />,
+};
+
+const GradientSlider = withStyles({
+  rail: {
+    opacity: 0.1,
+  },
+  track: {
+    opacity: 0.1,
+  },
+})(Slider);
+
 const GradientPicker = ({ color, label }: IGradientPickerProps) => {
   const classes = useStyles();
 
-  const [sliderPositions, setSliderPositions] = useState<number[]>([]);
-  const [editingPoint, setEditingPoint] = useState<number | undefined>();
   const [gradient, setGradient] = useState(DEFAULT_GRADIENT);
+  const [editingPoint, setEditingPoint] = useState<number | undefined>();
+  const [gradientTypesMenu, showGradientTypesMenu] = useState(false);
+  const [gradientTypesAnchor, setAnchor] = useState<null | HTMLElement>(null);
 
-  useEffect(() => {
-    console.log('GRADIENT CHANGED: ', gradient);
-  });
+  useEffect(() => {});
 
   const setColor = (color?: ColorResult, pointIndex?: number) => {
-    console.log(color, pointIndex);
     if (!((pointIndex as number) >= 0)) {
       return;
     }
@@ -114,12 +201,12 @@ const GradientPicker = ({ color, label }: IGradientPickerProps) => {
       rgb = gradient.points[pointIndex as number].color;
     }
 
+    const sliderPositions = _.map(gradient.points, 'position') || [];
+
     const point: IGradientPoint = {
       position: sliderPositions[pointIndex as number],
       color: rgb,
     };
-
-    console.log('POINT: ', pointIndex, point);
 
     const newPoints = gradient.points.map((p, index) => {
       if (index === pointIndex) {
@@ -134,6 +221,36 @@ const GradientPicker = ({ color, label }: IGradientPickerProps) => {
     });
   };
 
+  const addNewPoint = () => {
+    const newPoint: IGradientPoint = {
+      position: 50,
+      color: BLACK,
+    };
+
+    const points = gradient.points;
+    const newPointIndex = Math.round(points.length / 2);
+    points.splice(newPointIndex, 0, newPoint);
+
+    const newGradient = {
+      direction: gradient.direction,
+      points,
+    };
+
+    setGradient(newGradient);
+    setEditingPoint(newPointIndex);
+  };
+
+  const deletePoint = (position: number) => {
+    const points = _.filter(gradient.points, (p, index) => {
+      return index !== position;
+    });
+
+    setGradient({
+      direction: gradient.direction,
+      points,
+    });
+  };
+
   const rgbaFromGradientPoint = (point: IGradientPoint) => {
     return `rgba(${point.color.r},${point.color.g},${point.color.b},${
       point.color.a || 1
@@ -141,7 +258,6 @@ const GradientPicker = ({ color, label }: IGradientPickerProps) => {
   };
 
   const gradientToCss = (gradient: IGradientSpec) => {
-    console.log(gradient);
     let browserParam = 'linear-gradient';
     if (gradient.direction === 'ellipse at center') {
       browserParam = 'radial-gradient';
@@ -155,24 +271,28 @@ const GradientPicker = ({ color, label }: IGradientPickerProps) => {
       ', ',
     )})`;
 
-    console.log('CSS: ', css);
     return css;
   };
 
-  const handleSliderChange = (event: any, value: any) => {
-    console.log(value);
-    const previousPositions = sliderPositions;
-    setSliderPositions(value as number[]);
+  const setGradientDirection = (value: GradientDirection) => {
+    const newGradient = gradient;
+    newGradient.direction = value;
+    setGradient(newGradient);
+    showGradientTypesMenu(false);
+  };
 
-    let changedIndex = -1;
-    _.each(previousPositions, (pos: number, index: number) => {
-      if (pos !== value[index]) {
-        changedIndex = index;
+  const handleSliderChange = (event: any, newPositions: any) => {
+    const newGradient = gradient;
+
+    newGradient.points = gradient.points.map((p, index) => {
+      if (p.position !== newPositions[index]) {
+        p.position = newPositions[index];
       }
+      return p;
     });
 
-    setEditingPoint(changedIndex);
-    setColor(undefined, changedIndex);
+    setGradient(newGradient);
+    setColor(undefined, editingPoint);
   };
 
   return (
@@ -182,19 +302,128 @@ const GradientPicker = ({ color, label }: IGradientPickerProps) => {
         mt={2}
         width="100%"
         height={100}
+        className={classes.relativeContainer}
         style={{
           background: gradientToCss(gradient),
-        }}
-      />
-      <Slider
-        track={false}
-        onChangeCommitted={handleSliderChange}
-        defaultValue={_.map(gradient.points, 'position') || []}
-      />
+        }}>
+        <Tooltip title="Add a color stop">
+          <IconButton
+            className={classes.addButton}
+            size="small"
+            onClick={addNewPoint}>
+            <Add />
+          </IconButton>
+        </Tooltip>
 
-      <Box mt={5} mb={1} mx="auto">
+        <Tooltip title="Gradient Direction">
+          <IconButton
+            className={classes.gradientTypeButton}
+            size={'small'}
+            onClick={(e) => {
+              showGradientTypesMenu(true);
+              setAnchor(e.currentTarget);
+            }}>
+            {directionIconMap[gradient.direction]}
+          </IconButton>
+        </Tooltip>
+        <Menu
+          anchorEl={gradientTypesAnchor}
+          keepMounted={true}
+          open={gradientTypesMenu}
+          onClose={() => showGradientTypesMenu(false)}>
+          <MenuItem
+            key={'to bottom'}
+            selected={false}
+            onClick={() => setGradientDirection('to bottom')}>
+            Vertical
+          </MenuItem>
+          <MenuItem
+            key={'to right'}
+            selected={false}
+            onClick={() => setGradientDirection('to right')}>
+            Horizontal
+          </MenuItem>
+          <MenuItem
+            key={'45deg'}
+            selected={false}
+            onClick={() => setGradientDirection('45deg')}>
+            45 deg
+          </MenuItem>
+          <MenuItem
+            key={'135deg'}
+            selected={false}
+            onClick={() => setGradientDirection('135deg')}>
+            -45 deg
+          </MenuItem>
+          <MenuItem
+            key={'ellipse at center'}
+            selected={false}
+            onClick={() => setGradientDirection('ellipse at center')}>
+            Radial
+          </MenuItem>
+        </Menu>
+      </Box>
+      <Box className={classes.relativeContainer}>
+        <GradientSlider
+          track={false}
+          ThumbComponent={(props: any) => {
+            const index = props['data-index'];
+            const point = gradient.points[index];
+
+            return (
+              <div
+                {...props}
+                onMouseDown={() => {
+                  setEditingPoint(index as number);
+                }}
+                onMouseUp={() => {
+                  setEditingPoint(index as number);
+                }}
+                className={classes.point}
+                style={{
+                  ...props.style,
+                  backgroundColor: rgbaFromGradientPoint(point),
+                  border:
+                    index === editingPoint ? 'inset 1px #666' : 'transparent',
+                }}
+              />
+            );
+          }}
+          step={5}
+          onChangeCommitted={handleSliderChange}
+          onChange={handleSliderChange}
+          defaultValue={_.map(DEFAULT_GRADIENT.points, 'position') || []}
+          value={_.map(gradient.points, 'position') || []}
+        />
+      </Box>
+
+      <Box className={classes.relativeContainer}>
+        {gradient.points.map((p, index) => {
+          return (
+            <div key={index}>
+              {editingPoint === index && gradient.points.length >= 3 && (
+                <Tooltip title="Delete color stop">
+                  <IconButton
+                    size="small"
+                    style={{
+                      top: 0,
+                      left: `calc(${p.position}% - 9px)`,
+                    }}
+                    className={classes.thumbIcon}
+                    onClick={() => deletePoint(index)}>
+                    <Delete />
+                  </IconButton>
+                </Tooltip>
+              )}
+            </div>
+          );
+        })}
+      </Box>
+
+      <Box className={classes.relativeContainer} mt={5} mb={1} mx="auto">
         {(editingPoint as number) >= 0 && (
           <SketchPicker
+            className={classes.sketchPicker}
             color={rgbaFromGradientPoint(
               gradient.points[editingPoint as number],
             )}
