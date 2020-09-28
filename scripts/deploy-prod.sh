3c7f8a46d1dceffa12c37e5dabd3d6754cb25544
bluesea0109 <ub2099236@gmail.com>
2020-09-28 13:47:24 -0600

    Configure prod deploy.

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -5,12 +5,13 @@ set -e pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
-gcp_project="bavard-prd"
+gcp_project="bavard-prod"
 
 export REACT_APP_PROJECT_ID=$gcp_project
 export REACT_APP_ENV=prod
 
-npm ci
+rm -rf build
+npm install
 npm run build
 
-firebase deploy -f --token "$FIREBASE_TOKEN" --project $gcp_project --only hosting:bavard-ai
+firebase deploy -f --token "$FIREBASE_TOKEN" --project $gcp_project --only hosting:bavard-prod
