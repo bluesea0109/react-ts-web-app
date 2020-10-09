821ab8ac136c24d4af74006ca9362abb731eb74a
Samuel Jon <samuelwebmob2997@gmail.com>
2020-10-09 12:02:22 -0500

    updated nav item

--- src/components/IconButtons/NavItem.tsx
+++ src/components/IconButtons/NavItem.tsx
@@ -49,7 +49,6 @@ const NavItem = (props: NavItemProps) => {
 
   let Icon = null;
 
-  console.log('Current Icon status ??? ', keyVal);
   switch (props.css) {
     case 'Dashboard':
       Icon = <Dashboard active={active} />;
