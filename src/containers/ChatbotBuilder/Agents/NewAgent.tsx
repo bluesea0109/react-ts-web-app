13a8fd084e49450dd2173c3905903716e82c7816
bluesea0109 <ub2099236@gmail.com>
2020-10-19 19:35:54 -0600

    Lint

--- src/containers/ChatbotBuilder/Agents/NewAgent.tsx
+++ src/containers/ChatbotBuilder/Agents/NewAgent.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: '100%',
       backgroundColor: theme.palette.background.default,
     },
-  })
+  }),
 );
 
 interface INewAgentProps {
