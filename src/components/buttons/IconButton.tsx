feac3bf2089ac35d33c58d708d71825d1380de5e
bluesea0109 <ub2099236@gmail.com>
2020-10-24 11:20:36 -0700

    Changed the basic IConButton component

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -1,27 +1,17 @@
-import { Button, makeStyles, SvgIconTypeMap, Theme, Typography } from '@material-ui/core';
+import { Button, makeStyles, SvgIconTypeMap, Theme } from '@material-ui/core';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 import React from 'react';
 import { ButtonColorTypes, ButtonVariantTypes, TextTransformTypes } from '../types';
 
 interface CustomStyles {
   textTransform: TextTransformTypes;
-  iconPosition: 'left'|'right';
 }
 
 const useStyles = ({
   textTransform,
-  iconPosition,
 }: CustomStyles) => makeStyles((theme: Theme) => ({
   root: {
     textTransform,
-
-    '&.MuiButton-text': {
-      padding: '8px 0px',
-    },
-  },
-  typography: {
-    marginLeft: iconPosition === 'left' ? theme.spacing(1) : 0,
-    marginRight: iconPosition === 'right' ? theme.spacing(1) : 0,
   },
 }));
 
@@ -46,7 +36,6 @@ const IconButton: React.FC<IconButtonProps> = ({
 }) => {
   const classes = useStyles({
     textTransform: textTransform || 'none',
-    iconPosition: iconPosition || 'left',
   })();
 
   return (
@@ -55,10 +44,10 @@ const IconButton: React.FC<IconButtonProps> = ({
       variant={variant || 'contained'}
       className={classes.root}
       onClick={onClick}
+      startIcon={iconPosition === 'left' ? <Icon /> : undefined}
+      endIcon={iconPosition === 'right' ? <Icon /> : undefined}
     >
-      {iconPosition !== 'right' && <Icon />}
-      <Typography className={classes.typography}>{title}</Typography>
-      {iconPosition === 'right' && <Icon />}
+      {title}
     </Button>
   );
 };
