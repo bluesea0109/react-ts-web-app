3d46bfcf7e07512e02968a14bee563b0ebd5b162
Samuel Jon <samuelwebmob2997@gmail.com>
2020-10-30 10:35:50 -0500

    changed the progress gif

--- src/containers/ContentLoading.tsx
+++ src/containers/ContentLoading.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core';
+import { Grid, CircularProgress } from '@material-ui/core';
 import React from 'react';
 
 interface ContentLoadingProps {
@@ -8,9 +8,8 @@ interface ContentLoadingProps {
 function ContentLoading({shrinked}: ContentLoadingProps) {
   console.log('shrinked');
   return (
-    <Grid container={true} direction="column" justify="center" alignItems="center" style={{ position: 'absolute', width: shrinked ? 'clac(100% - 300px)' : '100%', backgroundColor: '#77777744', height: '100vh', top: '0px', left: shrinked ? '200px' : '0px'}}>
-      <img src="/cogs.gif" alt="loading" width="80px" height="80px"/>
-      {/* <CircularProgress color="secondary"/> */}
+    <Grid container={true} direction="column" justify="center" alignItems="center" style={{ position: 'absolute', width: shrinked ? 'clac(100% - 300px)' : '100%', backgroundColor: '#77777744', height: '100vh', top: '0px', left: shrinked ? '200px' : '0px'}}>      
+      <CircularProgress color="secondary"/>
     </Grid>
   );
 }
