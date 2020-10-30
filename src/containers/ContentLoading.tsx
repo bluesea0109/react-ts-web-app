416da98ae917babf0f609aec10d86f9a0fbc5de3
Samuel Jon <samuelwebmob2997@gmail.com>
2020-10-30 11:00:26 -0500

    removed the console

--- src/containers/ContentLoading.tsx
+++ src/containers/ContentLoading.tsx
@@ -5,8 +5,7 @@ interface ContentLoadingProps {
   shrinked?: boolean;
 }
 
-function ContentLoading({shrinked}: ContentLoadingProps) {
-  console.log('shrinked');
+function ContentLoading({shrinked}: ContentLoadingProps) {  
   return (
     <Grid container={true} direction="column" justify="center" alignItems="center" style={{ position: 'absolute', width: shrinked ? 'clac(100% - 300px)' : '100%', backgroundColor: '#77777744', height: '100vh', top: '0px', left: shrinked ? '200px' : '0px'}}>
       <CircularProgress color="secondary"/>
