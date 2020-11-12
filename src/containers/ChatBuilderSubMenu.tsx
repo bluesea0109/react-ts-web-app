66e4b2444a4a05b498ff19d94524b621798fbbbc
Samuel Jon <samuelwebmob2997@gmail.com>
2020-11-12 13:25:42 -0600

    fixed tab size

--- src/containers/ChatBuilderSubMenu.tsx
+++ src/containers/ChatBuilderSubMenu.tsx
@@ -1,4 +1,10 @@
-import { createStyles, List, ListItem, Theme, Typography } from '@material-ui/core';
+import {
+  createStyles,
+  List,
+  ListItem,
+  Theme,
+  Typography,
+} from '@material-ui/core';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
@@ -13,19 +19,26 @@ interface BotSubMenuProps {
   getAgentPath: (item: string) => string;
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  listItem: {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    listItem: {
       color: 'white',
       padding: '0px 0px 0px 30px',
       marginRight: '15px',
-  },
-  blank: {
-    height: '40px',
-    marginLeft: '90px',
-  },
-}));
+    },
+    blank: {
+      height: '40px',
+      marginLeft: '90px',
+    },
+  }),
+);
 
-const BotSubMenu = ({title, items, category, getAgentPath}: BotSubMenuProps) => {
+const BotSubMenu = ({
+  title,
+  items,
+  category,
+  getAgentPath,
+}: BotSubMenuProps) => {
   const classes = useStyles();
   const location = useLocation();
   const selectedStyle = {
@@ -38,57 +51,67 @@ const BotSubMenu = ({title, items, category, getAgentPath}: BotSubMenuProps) => 
   const selectedPath = location.pathname.split('/').reverse()[1];
 
   const selectedCategory = () => {
-    if (botSubMenuData.configure.items.findIndex((item) => {
-      return item.path === selectedPath;
-    }) !== -1) {
+    if (
+      botSubMenuData.configure.items.findIndex((item) => {
+        return item.path === selectedPath;
+      }) !== -1
+    ) {
       return 'Configure';
-    } else if (botSubMenuData.training.items.findIndex((item) => {
-      return item.path === selectedPath;
-    }) !== -1) {
+    } else if (
+      botSubMenuData.training.items.findIndex((item) => {
+        return item.path === selectedPath;
+      }) !== -1
+    ) {
       return 'Training';
-    } else if (botSubMenuData.launch.items.findIndex((item) => {
-      return item.path === selectedPath;
-    }) !== -1) {
+    } else if (
+      botSubMenuData.launch.items.findIndex((item) => {
+        return item.path === selectedPath;
+      }) !== -1
+    ) {
       return 'Launch';
     }
   };
 
-  const renderItems = items.map((item: any, index: number) => <ListItem
-    component={Link}
-    key={index}
-    to={getAgentPath(item.path)}
-    selected={
-      location.pathname.includes('projects') &&
-      location.pathname.includes(item.path)
-    }
-    button={true}
-    className={classes.listItem}>
-    <ListItemIcon style={{ color: 'white'}}>
-      <SubMenuIcon title="Project" active={false}/>
-    </ListItemIcon>
-    <ListItemText
-      primary={item.title}
-      style={
+  const renderItems = items.map((item: any, index: number) => (
+    <ListItem
+      component={Link}
+      key={index}
+      to={getAgentPath(item.path)}
+      selected={
         location.pathname.includes('projects') &&
         location.pathname.includes(item.path)
-          ? selectedStyle
-          : {paddingLeft: '20px'}
       }
-    />
-    </ListItem>,
-  );
+      button={true}
+      className={classes.listItem}>
+      <ListItemIcon style={{ color: 'white' }}>
+        <SubMenuIcon title="Project" active={false} />
+      </ListItemIcon>
+      <ListItemText
+        primary={item.title}
+        style={
+          location.pathname.includes('projects') &&
+          location.pathname.includes(item.path)
+            ? selectedStyle
+            : { paddingLeft: '20px' }
+        }
+      />
+    </ListItem>
+  ));
 
-  return (<List>
-    <Typography
-      className={classes.blank}
-      style={category === selectedCategory() ?
-        {color: '#4a90e2', fontWeight: 'bold'}
-        : {color: 'white', fontWeight: 'bold'}}
-    >
-      {title}
-    </Typography>
-    {renderItems}
-  </List>);
+  return (
+    <List>
+      <Typography
+        className={classes.blank}
+        style={
+          category === selectedCategory()
+            ? { color: '#4a90e2', fontWeight: 'bold' }
+            : { color: 'white', fontWeight: 'bold' }
+        }>
+        {title}
+      </Typography>
+      {renderItems}
+    </List>
+  );
 };
 
 export default BotSubMenu;
