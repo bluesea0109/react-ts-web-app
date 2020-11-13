0f87ff7e312a268559b1294037602a9330e10319
bluesea0109 <ub2099236@gmail.com>
2020-11-13 10:32:49 -0800

    Refactored CategorySets using Common Table

--- src/containers/ImageLabeling/CategorySets/CategorySets.tsx
+++ src/containers/ImageLabeling/CategorySets/CategorySets.tsx
@@ -1,15 +1,9 @@
 import { useQuery } from '@apollo/client';
 import { Typography } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Toolbar from '@material-ui/core/Toolbar';
 import React from 'react';
 import { useParams } from 'react-router';
@@ -17,10 +11,10 @@ import { GET_CATEGORY_SETS } from '../../../common-gql-queries';
 import ContentLoading from '../../ContentLoading';
 import CreateCategorySetDialog from './CreateCategorySetDialog';
 import DeleteCategorySetDialog from './DeleteCategorySetDialog';
+import { CommonTable } from '../../../components';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {},
     toolbar: {
       paddingLeft: theme.spacing(2),
     },
@@ -47,56 +41,52 @@ function CategorySets() {
   }
 
   const catSets = categorySets.data.ImageLabelingService_categorySets;
+
+  const columns = [
+    { title: 'Name', field: 'name' },
+    { title: 'Id', field: 'id' },
+    {
+      title: 'Categories',
+      field: 'categories',
+      renderRow: (rowData: any) => (
+        <List className={classes.list}>
+          {rowData.categories.map((cat: any, i: number) => (
+            <ListItem key={i}>
+              <ListItemText primary={cat.name} />
+            </ListItem>
+          ))}
+        </List>
+      ),
+    },
+    {
+      title: 'Delete',
+      renderRow: (rowData: any) => (
+        <DeleteCategorySetDialog
+          name={rowData.name}
+          categorySetId={rowData.id}
+        />
+      ),
+    },
+  ];
   return (
-    <Grid container={true} className={classes.root}>
-      <Toolbar
-        variant="dense"
-        disableGutters={true}
-        className={classes.toolbar}>
-        <Typography variant="h6">{'Category Sets'}</Typography>
-        <Typography style={{ padding: 2 }} />
-        <CreateCategorySetDialog />
-      </Toolbar>
-      <Grid container={true} item={true} xs={12}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Id</TableCell>
-              <TableCell>Categories</TableCell>
-              <TableCell>Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {catSets.map((set: any, i: number) => {
-              return (
-                <TableRow key={i} hover={true}>
-                  <TableCell component="th" scope="row">
-                    {set.name}
-                  </TableCell>
-                  <TableCell>{set.id}</TableCell>
-                  <TableCell>
-                    <List className={classes.list}>
-                      {set.categories.map((cat: any, i: number) => (
-                        <ListItem key={i}>
-                          <ListItemText primary={cat.name} />
-                        </ListItem>
-                      ))}
-                    </List>
-                  </TableCell>
-                  <TableCell>
-                    <DeleteCategorySetDialog
-                      name={set.name}
-                      categorySetId={set.id}
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </Grid>
-    </Grid>
+    <CommonTable
+      data={{
+        columns,
+        rowsData: catSets,
+      }}
+      components={{
+        Toolbar: () => (
+          <Toolbar
+            variant="dense"
+            disableGutters={true}
+            className={classes.toolbar}>
+            <Typography variant="h6">{'Category Sets'}</Typography>
+            <Typography style={{ padding: 2 }} />
+            <CreateCategorySetDialog />
+          </Toolbar>
+        ),
+      }}
+    />
   );
 }
 
