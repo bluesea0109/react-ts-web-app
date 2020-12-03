1edda5c9859015277ed3ded44cf9a367a865dbd5
bluesea0109 <ub2099236@gmail.com>
2020-12-03 12:05:15 -0800

    Billing Bag Icon updated in svg

--- src/icons/BillingBagIcon.tsx
+++ src/icons/BillingBagIcon.tsx
@@ -9,8 +9,8 @@ export default function BillingBagIcon() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M3 18.8571C1.34315 18.8571 0 17.514 0 15.8571V7.60715H24V15.8571C24 17.514 22.6569 18.8571 21 18.8571H3ZM5.14286 10.1786C4.25526 10.1786 3.53571 10.8981 3.53571 11.7857V12.8571C3.53571 13.7447 4.25526 14.4643 5.14286 14.4643H7.71429C8.60189 14.4643 9.32143 13.7447 9.32143 12.8571V11.7857C9.32143 10.8981 8.60189 10.1786 7.71429 10.1786H5.14286Z"
         fill="black"
       />
